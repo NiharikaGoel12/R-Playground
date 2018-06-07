bf1007ae9c711ef9dca95f59970ba20c610efbab
Niharika Goel <niharika.coer@gmail.com>
2018-06-07 09:10:21 +0530

    Add snippet to merge multiple CSV in a folder

---
+++ MergeCSVs.R
@@ -0,0 +1,23 @@
+library(openxlsx)
+library(dplyr)
+library(stringr)
+library(gdata)
+library(grepl)
+setwd("/Users/niharikagoel/Downloads/Invoice_Store/results")
+merge_file_name <- "merged_file.xlsx"
+# for csv read.csv(i, headers=TRUE)
+
+filenames <- list.files(full.names=TRUE)
+
+All <- lapply(filenames,function(i){
+  if(grepl(merge_file_name,i)) {
+    print("ignoring final merge  file")
+  } 
+  else {
+    print(paste("Merging",i,sep = " "))
+    read.xlsx(i)
+  }
+})
+
+df <- do.call(rbind.data.frame, All)
+write.xlsx(df,merge_file_name)
